e913f4404b5904125b50cf37f9c196c01bded811
joeywire <61367542+joeywire@users.noreply.github.com>
2020-12-14 20:29:30 -0600

    got some hash and salt going on

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,8 +3,18 @@ const bcrypt = require('bcryptjs');
 
 const User = require('../users/users-model');
 
-router.post("/register", (req, res) => {
-  res.status(200).json({ sure: "thing boss"});
+router.post("/register", async (req, res) => {
+  const creds = req.body; 
+  const rounds = process.env.HASH_ROUNDS || 4;
+  const hash = bcrypt.hashSync(creds.password, rounds);
+  creds.password = hash; 
+
+  try { 
+    const savedData = await User.add(creds); 
+    res.status(200).json({ message: savedData });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 });
 
 module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,17 +1,25 @@
 const express = require('express'); 
 const morgan = require('morgan'); 
 const helmet = require('helmet');
+const cors = require('cors'); 
+const session = require('express-session'); 
+//Will Return a CoNsTrucToR
+const KnexSessionStore = require('connect-session-knex')(session); 
 
 const userRouter = require('./users/users-router');
 const authRouter = require('./auth/auth-router');
+const dbConfig = require('../database/dbConfig');  
 
 
 const server = express();
 
+//GLOBAL MIDDLEWARE
 server.use(express.json());
 server.use(helmet());
 server.use(morgan('tiny')); 
+server.use(cors()); 
 
+//ROUTES
 server.use("/api/users", userRouter); 
 server.use("/api/auth", authRouter); 
 
